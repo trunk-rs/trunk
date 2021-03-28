efda0366724401410e848a2a36e0131af8293c6a
Philip Peterson <philip.c.peterson@gmail.com>
2021-03-28 13:54:10 -0700

    Add Vanilla Rust example

---
+++ examples/vanilla/src/main.rs
@@ -0,0 +1,26 @@
+#![recursion_limit = "1024"]
+
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+use console_error_panic_hook::set_once as set_panic_hook;
+use wasm_bindgen::prelude::*;
+use web_sys::{window, Document, Window};
+
+fn start_app() {
+    let document = window().and_then(|win| win.document()).expect("Could not access document");
+    let body = document.body().expect("Could not access document.body");
+    let text_node = document.create_text_node("Hello, world from Vanilla Rust!");
+    body.append_child(text_node.as_ref()).expect("Failed to append text");
+}
+
+#[wasm_bindgen(inline_js = "export function snippetTest() { console.log('Hello from JS FFI!'); }")]
+extern "C" {
+    fn snippetTest();
+}
+
+fn main() {
+    set_panic_hook();
+    snippetTest();
+    start_app();
+}
