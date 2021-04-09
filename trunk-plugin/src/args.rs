a03ccd4f0fa6d39e0305c163cdb3b8c195f38b67
Dzenan Jupic <56133904+DzenanJupic@users.noreply.github.com>
2021-04-09 23:26:12 +0200

    Added the ability to extract the plugin arguments from HTML attributes

--- trunk-plugin/src/args.rs
+++ trunk-plugin/src/args.rs
@@ -1,14 +1,49 @@
+use std::collections::HashMap;
+
 use serde_json::{Map, Value};
+
+use crate::Permissions;
 
 #[derive(Clone, Debug, Default, serde::Deserialize, serde::Serialize)]
 pub struct Args {
     pub user_arguments: Map<String, Value>,
+    pub permissions: Permissions,
 }
 
 impl Args {
+    const PLUGIN_ARGS_ATTR: &'static str = "data-args";
+    const PLUGIN_ARG_ATTR_PREFIX: &'static str = "data-";
+
+    pub fn from_link_attrs(mut link_attrs: HashMap<String, String>) -> serde_json::Result<Self> {
+        let mut user_arguments = link_attrs
+            .remove(Self::PLUGIN_ARGS_ATTR)
+            .as_deref()
+            .map(serde_json::from_str::<Map<String, Value>>)
+            .unwrap_or_else(|| Ok(Map::new()))?;
+
+        link_attrs
+            .drain()
+            .filter(|(k, _)| k.starts_with(Self::PLUGIN_ARG_ATTR_PREFIX))
+            .try_for_each(|(k, v)| {
+                let v = serde_json::from_str::<Value>(&v)?;
+                user_arguments.insert(k, v);
+                Ok(())
+            })?;
+
+        Ok(Self {
+            user_arguments,
+            permissions: Permissions::NONE,
+        })
+    }
+
+    pub fn with_permissions(mut self, permissions: Permissions) -> Self {
+        self.permissions.insert(permissions);
+        self
+    }
+
     pub fn parse_user_arguments<T>(self) -> serde_json::Result<T>
-    where
-        T: serde::de::DeserializeOwned,
+        where
+            T: serde::de::DeserializeOwned,
     {
         serde_json::from_value(Value::Object(self.user_arguments))
     }
