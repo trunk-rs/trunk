3bf8590ec7e55eeda5e455d01465fcf2f6dfc212
Web Cerebrium <viclopata@gmail.com>
2021-08-20 19:51:22 +0300

    Update src/config/models.rs
    Co-authored-by: Anthony Dodd <2380740+thedodd@users.noreply.github.com>

--- src/config/models.rs
+++ src/config/models.rs
@@ -48,12 +48,19 @@ pub struct ConfigOptsBuild {
     pub pattern_preload: Option<String>,
     #[structopt(skip)]
     #[serde(default)]
-    /// Can be read only from config file
-    /// Optional parameters of replacements inside
-    /// While {var} is being replaced with the provided value,
-    /// {@path} is replaced with contents of the provided file.
-    /// This allows insertion of some big JSON state or even HTML files
-    /// as a part of the `index.html` build
+    /// Optional replacement parameters corresponding to the patterns provided in
+    /// `pattern_script` and `pattern_preload`.
+    ///
+    /// When a pattern is being replaced with its corresponding value from this map, if the value is
+    /// prefixed with the symbol `@`, then the value is expected to be a file path, and the pattern
+    /// will be replaced with the contents of the target file. This allows insertion of some big JSON
+    /// state or even HTML files as a part of the `index.html` build.
+    ///
+    /// Trunk will automatically insert the `base`, `wasm` and `js` key/values into this map. In order
+    //// for the app to be loaded properly, the patterns `{base}`, `{wasm}` and `{js}` should be used
+    /// in `pattern_script` and `pattern_preload`.
+    ///
+    /// These values can only be provided via config file.
     pub pattern_params: Option<HashMap<String, String>>,
 }
 
