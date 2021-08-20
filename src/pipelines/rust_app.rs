6dc6bf587ac9083bbbe26b1410837a88fce535bc
Web Cerebrium <viclopata@gmail.com>
2021-08-20 19:43:08 +0300

    Update src/pipelines/rust_app.rs
    Co-authored-by: Anthony Dodd <2380740+thedodd@users.noreply.github.com>

--- src/pipelines/rust_app.rs
+++ src/pipelines/rust_app.rs
@@ -311,10 +311,7 @@ impl RustAppOutput {
     pub async fn finalize(self, dom: &mut Document) -> Result<()> {
         let (base, js, wasm, head, body) = (&self.cfg.public_url, &self.js_output, &self.wasm_output, "html head", "html body");
         let (pattern_script, pattern_preload) = (&self.cfg.pattern_script, &self.cfg.pattern_preload);
-        let mut params: HashMap<String, String> = match &self.cfg.pattern_params {
-            Some(x) => x.clone(),
-            None => HashMap::new(),
-        };
+        let mut params = self.cfg.pattern_params.unwrap_or_default();
         params.insert("base".to_owned(), base.clone());
         params.insert("js".to_owned(), js.clone());
         params.insert("wasm".to_owned(), wasm.clone());
