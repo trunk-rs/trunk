cc9a8706d7575740bdfbc816a4ae5ead85ad604b
Anthony Dodd <dodd.anthonyjosiah@gmail.com>
2022-06-30 09:47:17 -0500

    Prep for 0.16.0 release
    - Updated lock deps.
    - A few small updates to webworker example.

--- examples/webworker/src/bin/app.rs
+++ examples/webworker/src/bin/app.rs
@@ -13,9 +13,16 @@ fn worker_new(name: &str) -> Worker {
         .expect("origin to be available");
 
     let script = Array::new();
-    script.push(&format!(r#"importScripts("{origin}/{name}.js");wasm_bindgen("{origin}/{name}_bg.wasm");"#).into());
+    script.push(
+        &format!(r#"importScripts("{origin}/{name}.js");wasm_bindgen("{origin}/{name}_bg.wasm");"#)
+            .into(),
+    );
 
-    let blob = Blob::new_with_str_sequence_and_options(&script, BlobPropertyBag::new().type_("text/javascript")).expect("blob creation succeeds");
+    let blob = Blob::new_with_str_sequence_and_options(
+        &script,
+        BlobPropertyBag::new().type_("text/javascript"),
+    )
+    .expect("blob creation succeeds");
 
     let url = Url::create_object_url_with_blob(&blob).expect("url creation succeeds");
 
@@ -50,11 +57,22 @@ fn main() {
             let msg = Array::new();
             msg.push(&2.into());
             msg.push(&5.into());
-            worker_clone.post_message(&msg.into()).expect("sending message to succeed");
+            worker_clone
+                .post_message(&msg.into())
+                .expect("sending message to succeed");
         } else {
-            let a = data.get(0).as_f64().expect("first array value to be a number") as u32;
-            let b = data.get(1).as_f64().expect("second array value to be a number") as u32;
-            let result = data.get(2).as_f64().expect("third array value to be a number") as u32;
+            let a = data
+                .get(0)
+                .as_f64()
+                .expect("first array value to be a number") as u32;
+            let b = data
+                .get(1)
+                .as_f64()
+                .expect("second array value to be a number") as u32;
+            let result = data
+                .get(2)
+                .as_f64()
+                .expect("third array value to be a number") as u32;
 
             web_sys::console::log_1(&format!("{a} x {b} = {result}").into());
         }
--- examples/webworker/src/bin/worker.rs
+++ examples/webworker/src/bin/worker.rs
@@ -16,8 +16,14 @@ fn main() {
         web_sys::console::log_1(&"got message".into());
 
         let data = Array::from(&msg.data());
-        let a = data.get(0).as_f64().expect("first array value to be a number") as u32;
-        let b = data.get(1).as_f64().expect("second array value to be a number") as u32;
+        let a = data
+            .get(0)
+            .as_f64()
+            .expect("first array value to be a number") as u32;
+        let b = data
+            .get(1)
+            .as_f64()
+            .expect("second array value to be a number") as u32;
 
         data.push(&(a * b).into());
         scope_clone
@@ -27,7 +33,7 @@ fn main() {
     scope.set_onmessage(Some(onmessage.as_ref().unchecked_ref()));
     onmessage.forget();
 
-    // the worker must send a message to indicate that it's ready to receive messages
+    // The worker must send a message to indicate that it's ready to receive messages.
     scope
         .post_message(&Array::new().into())
         .expect("posting ready message succeeds");
