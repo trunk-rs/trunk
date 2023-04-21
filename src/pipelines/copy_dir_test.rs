f15f5546cc1b955d15803af883d0a510b56face2
William Tange <will@brt.sexy>
2023-04-21 13:23:52 +0200

    Add passing test case for data-target-path attr

--- src/pipelines/copy_dir_test.rs
+++ src/pipelines/copy_dir_test.rs
@@ -99,3 +99,44 @@ async fn ok_run_basic_copy() -> Result<()> {
 
     Ok(())
 }
+
+#[tokio::test]
+async fn ok_run_target_path_copy() -> Result<()> {
+    // Assemble.
+    let (tmpdir, cfg, asset_dir) = setup_test_config().await?;
+    let copy_location_dir = cfg.staging_dist.join("not-test_dir");
+    let mut attrs = HashMap::new();
+    attrs.insert(ATTR_HREF.into(), "test_dir".into());
+    attrs.insert("data-target-path".into(), "not-test_dir".into());
+    let cmd = CopyDir::new(cfg, Arc::new(tmpdir.into_path()), attrs, 0)
+        .await
+        .context("error constructing CopyDir pipeline")?;
+
+    // Action.
+    let _out = cmd
+        .spawn()
+        .await
+        .context("unexpected task join error from pipeline")?
+        .context("unexpected pipeline error")?;
+
+    // Assert.
+    let orig_file = tokio::fs::read_to_string(asset_dir.join("test_file"))
+        .await
+        .context("error reading original file")?;
+    let copied_file = tokio::fs::read_to_string(copy_location_dir.join("test_file"))
+        .await
+        .context("error reading copied file")?;
+    anyhow::ensure!(
+        copy_location_dir.is_dir(),
+        "expected '{}' to be a directory",
+        copy_location_dir.display(),
+    );
+    anyhow::ensure!(
+        orig_file == copied_file,
+        "unexpected content after copy, expected '{}' == '{}'",
+        orig_file,
+        copied_file
+    );
+
+    Ok(())
+}
