dbdbe8c9a1b4aad973e993d0c4359885ac5c9e24
Jens Reimann <ctron@dentrassi.de>
2023-09-18 10:47:42 +0200

    feat: allow disabling the cooldown

--- src/config/rt.rs
+++ src/config/rt.rs
@@ -187,8 +187,10 @@ pub struct RtcWatch {
     pub paths: Vec<PathBuf>,
     /// Paths to ignore.
     pub ignored_paths: Vec<PathBuf>,
-    /// Use polling mode for detecting chnages
+    /// Use polling mode for detecting changes
     pub poll: bool,
+    /// Allow disabling the cooldown
+    pub ignore_cooldown: bool,
 }
 
 impl RtcWatch {
@@ -237,6 +239,7 @@ impl RtcWatch {
             paths,
             ignored_paths,
             poll: opts.poll,
+            ignore_cooldown: opts.ignore_cooldown,
         })
     }
 }
