a936a1babe4673ca41cede129c9c3bdafcc8e062
Jens Reimann <ctron@dentrassi.de>
2023-10-20 16:55:11 +0200

    chore: clean up the auto-reload script a bit

--- src/autoreload.js
+++ src/autoreload.js
@@ -1,22 +1,24 @@
+"use strict";
+
 (function () {
-    var protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
-    var url = protocol + '//' + '{{__TRUNK_ADDRESS__}}' + '/_trunk/ws';
-    var poll_interval = 5000;
-    var reload_upon_connect = () => {
+    const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
+    const url = protocol + '//' + '{{__TRUNK_ADDRESS__}}' + '/_trunk/ws';
+    const poll_interval = 5000;
+    const reload_upon_connect = () => {
         window.setTimeout(
             () => {
                 // when we successfully reconnect, we'll force a
                 // reload (since we presumably lost connection to
                 // trunk due to it being killed, so it will have
                 // rebuilt on restart)
-                var ws = new WebSocket(url);
+                const ws = new WebSocket(url);
                 ws.onopen = () => window.location.reload();
                 ws.onclose = reload_upon_connect;
             },
             poll_interval);
     };
 
-    var ws = new WebSocket(url);
+    const ws = new WebSocket(url);
     ws.onmessage = (ev) => {
         const msg = JSON.parse(ev.data);
         if (msg.reload) {
