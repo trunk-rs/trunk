28bc433f363d6e3fd1fb64435914925f396b858d
Jens Reimann <ctron@dentrassi.de>
2023-12-12 09:19:55 +0100

    style: remove a seemingly unused import

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -19,6 +19,6 @@ pub const STAGE_DIR: &str = ".stage";
 pub use manifest::CargoMetadata;
 pub use models::{
     ConfigOpts, ConfigOptsBuild, ConfigOptsClean, ConfigOptsHook, ConfigOptsProxy, ConfigOptsServe,
-    ConfigOptsTools, ConfigOptsWatch, CrossOrigin, CrossOriginParseError, WsProtocol,
+    ConfigOptsTools, ConfigOptsWatch, CrossOrigin, WsProtocol,
 };
 pub use rt::{Features, RtcBuild, RtcClean, RtcServe, RtcWatch};
