1dc6d7a6e126d0391b1f988dfb2e9564c078e438
Jens Reimann <ctron@dentrassi.de>
2024-01-26 15:04:56 +0100

    style: applied rustfmt

--- examples/vanilla/src/main.rs
+++ examples/vanilla/src/main.rs
@@ -5,10 +5,13 @@ use wasm_bindgen::prelude::*;
 use web_sys::window;
 
 fn start_app() {
-    let document = window().and_then(|win| win.document()).expect("Could not access document");
+    let document = window()
+        .and_then(|win| win.document())
+        .expect("Could not access document");
     let body = document.body().expect("Could not access document.body");
     let text_node = document.create_text_node("Hello, world from Vanilla Rust!");
-    body.append_child(text_node.as_ref()).expect("Failed to append text");
+    body.append_child(text_node.as_ref())
+        .expect("Failed to append text");
 }
 
 #[wasm_bindgen(inline_js = "export function snippetTest() { console.log('Hello from JS FFI!'); }")]
