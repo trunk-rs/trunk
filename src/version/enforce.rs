d950d3808e05aeb9d1e636377d3f5e5e10b0799f
Jens Reimann <ctron@dentrassi.de>
2024-04-12 09:24:38 +0200

    chore: add version tests, and fix ci
    Add some tests to validate version enforcement. Also fix the CI, as
    it looks like `0.19.0-alpha.1` is not intended to match other
    pre-release versions.

--- src/version/enforce.rs
+++ src/version/enforce.rs
@@ -34,3 +34,52 @@ pub(crate) fn enforce_version_with(required: &VersionReq, actual: Version) -> an
 
     Ok(())
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    // requires, actual, pass?
+    #[case("*", "0.19.0", true)]
+    #[case("*", "0.19.0-alpha.1", true)]
+    #[case("0.19", "0.19.0", true)]
+    #[case("0.19.0", "0.19.0", true)]
+    #[case("0.19.0", "0.19.1", true)]
+    // this may come unexpected, but for 0.x version, a minor version change is a breaking change
+    #[case("0.20.0", "0.19.0", false)]
+    #[case("0.19.0-alpha.2", "0.19.0-alpha.1", false)]
+    #[case("0.19.0-alpha.2", "0.19.0-alpha.2", true)]
+    #[case("0.19.0-alpha.2", "0.19.0-alpha.3", true)]
+    #[case("0.19.0-alpha.2", "0.19.0", true)]
+    #[case("0.19.0-alpha.2", "0.19.1", true)]
+    // this may come unexpected, but for 0.x version, a minor version change is a breaking change
+    #[case("0.19.0-alpha.2", "0.20.0", false)]
+    #[case("0.19.1", "0.19.0", false)]
+    #[case("0.19.1", "0.19.0-alpha.1", false)]
+    #[case("0.19.1", "0.19.1-alpha.1", false)]
+    #[case("0.20.0", "0.19.0-alpha.1", false)]
+    #[case("0.20.0", "0.19.0", false)]
+    #[case("0.20.0", "0.19.1-alpha.1", false)]
+    #[case("0.20.0", "0.19.1", false)]
+    // a way to say: 0.19.0 or greater
+    #[case(">=0.19.0", "0.19.0", true)]
+    #[case(">=0.19.0", "0.19.1", true)]
+    #[case(">=0.19.0", "0.20.0", true)]
+    // a way to say: 0.19.0-alpha.2 or greater
+    #[case(">=0.19.0-alpha.2", "0.19.0-alpha.1", false)]
+    #[case(">=0.19.0-alpha.2", "0.19.0-alpha.2", true)]
+    #[case(">=0.19.0-alpha.2", "0.19.0-rc.1", true)]
+    #[case(">=0.19.0-alpha.2", "0.19.0", true)]
+    // The following case comes unexpected
+    #[case(">=0.19.0-alpha.2", "0.20.0-alpha.1", false)]
+    #[case(">=0.19.0-alpha.2", "0.20.0", true)]
+    fn test_requires(
+        #[case] required: VersionReq,
+        #[case] actual: Version,
+        #[case] expected: bool,
+    ) {
+        assert_eq!(expected, enforce_version_with(&required, actual).is_ok());
+    }
+}
