d43bd126af3f826b715fee49e966915a4c3e4de4
Jens Reimann <ctron@dentrassi.de>
2024-05-23 09:16:52 +0200

    chore: update to yew 0.20 and ybc 0.4

--- examples/yew-tls/src/main.rs
+++ examples/yew-tls/src/main.rs
@@ -1,65 +1,44 @@
-#![recursion_limit = "1024"]
-
 use console_error_panic_hook::set_once as set_panic_hook;
 use wasm_bindgen::prelude::*;
+use web_sys::window;
 use ybc::TileCtx::{Ancestor, Child, Parent};
 use yew::prelude::*;
-use yew::services::ConsoleService;
-use yew::utils::window;
 
-struct App;
+#[function_component(App)]
+pub fn app() -> Html {
+    let location_service = LocationService {};
+    let location = location_service.get_location().href().unwrap_or_default();
+    html! {
+        <>
+        <ybc::Navbar
+            classes={classes!("is-success")}
+            padded=true
+            navbrand={html!{
+                <ybc::NavbarItem>
+                    <ybc::Title classes={classes!("has-text-white")} size={ybc::HeaderSize::Is4}>{"Location Demo"}</ybc::Title>
+                </ybc::NavbarItem>
+            }}
+        />
 
-impl Component for App {
-    type Message = ();
-    type Properties = ();
-
-    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
-        Self
-    }
-
-    fn update(&mut self, _: Self::Message) -> bool {
-        false
-    }
-
-    fn change(&mut self, _: Self::Properties) -> bool {
-        false
-    }
-
-    fn view(&self) -> Html {
-        let location_service = LocationService {};
-        let location = location_service.get_location().href().unwrap_or_default();
-        html! {
-            <>
-            <ybc::Navbar
-                classes=classes!("is-success")
-                padded=true
-                navbrand=html!{
-                    <ybc::NavbarItem>
-                        <ybc::Title classes=classes!("has-text-white") size=ybc::HeaderSize::Is4>{"Location Demo"}</ybc::Title>
-                    </ybc::NavbarItem>
-                }
-            />
-
-            <ybc::Hero
-                classes=classes!("is-light")
-                size=ybc::HeroSize::FullheightWithNavbar
-                body=html!{
-                    <ybc::Container classes=classes!("is-centered")>
-                    <ybc::Tile ctx=Ancestor>
-                        <ybc::Tile ctx=Parent size=ybc::TileSize::Twelve>
-                            <ybc::Tile ctx=Parent>
-                                <ybc::Tile ctx=Child classes=classes!("notification", "is-success")>
-                                    <ybc::Subtitle size=ybc::HeaderSize::Is3 classes=classes!("has-text-white")>{"Location"}</ybc::Subtitle>
-                                    <p>{"The current location is: "} {location}</p>
-                                </ybc::Tile>
+        <ybc::Hero
+            classes={classes!("is-light")}
+            size={ybc::HeroSize::FullheightWithNavbar}
+            body={html!{
+                <ybc::Container classes={classes!("is-centered")}>
+                <ybc::Tile ctx={Ancestor}>
+                    <ybc::Tile ctx={Parent} size={ybc::TileSize::Twelve}>
+                        <ybc::Tile ctx={Parent}>
+                            <ybc::Tile ctx={Child} classes={classes!("notification", "is-success")}>
+                                <ybc::Subtitle size={ybc::HeaderSize::Is3} classes={classes!("has-text-white")}>{"Location"}</ybc::Subtitle>
+                                <p>{"The current location is: "} {location}</p>
                             </ybc::Tile>
                         </ybc::Tile>
                     </ybc::Tile>
-                    </ybc::Container>
-                }>
-            </ybc::Hero>
-            </>
-        }
+                </ybc::Tile>
+                </ybc::Container>
+            }}>
+        </ybc::Hero>
+        </>
     }
 }
 
@@ -67,7 +46,7 @@ pub struct LocationService {}
 
 impl LocationService {
     pub fn get_location(&self) -> web_sys::Location {
-        window().location()
+        window().unwrap().location()
     }
 }
 
@@ -83,12 +62,12 @@ fn main() {
     // Show off some feature flag enabling patterns.
     #[cfg(feature = "demo-abc")]
     {
-        ConsoleService::log("feature `demo-abc` enabled");
+        web_sys::console::log_1(&"feature `demo-abc` enabled".into());
     }
     #[cfg(feature = "demo-xyz")]
     {
-        ConsoleService::log("feature `demo-xyz` enabled");
+        web_sys::console::log_1(&"feature `demo-xyz` enabled".into());
     }
 
-    yew::start_app::<App>();
+    yew::Renderer::<App>::new().render();
 }
