a2f105ef3dd62d82759a8f934c0bc6c401d4a9e7
Jens Reimann <ctron@dentrassi.de>
2026-02-27 08:48:15 +0100

    build: drop the build script

--- build.rs
+++
@@ -1,5 +0,0 @@
-extern crate built;
-
-fn main() {
-    built::write_built_file().expect("Failed to acquire build-time information");
-}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 #![deny(clippy::expect_used)]
 #![deny(clippy::unwrap_used)]
 
-pub mod built_info {
-    // The file has been placed there by the build script.
-    include!(concat!(env!("OUT_DIR"), "/built.rs"));
-}
 mod build;
 mod cmd;
 mod common;
